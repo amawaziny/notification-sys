4af818363b6722ff5fbfc06701a2d084f69b4412
amawaziny <amawaziny@gmail.com>
2021-06-26 16:41:05 +0200

    implement serializable

--- src/main/java/sa/nana/notification/models/FailureNotificationPayload.java
+++ src/main/java/sa/nana/notification/models/FailureNotificationPayload.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class FailureNotificationPayload {
+public class FailureNotificationPayload implements Serializable {
     private String exceptionMessage;
     private Payload payload;
 }
